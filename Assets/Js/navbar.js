f6d29d4046a73f6a2d5096f56b7c0c7baa43d679
Lennghc <l.m.esveld@gmail.com>
2022-12-16 12:17:44 +0100

    Home page content gevuld

--- Assets/Js/navbar.js
+++ Assets/Js/navbar.js
@@ -1,9 +1,7 @@
 const body = document.querySelector("body"),
     nav = document.querySelector("nav"),
     modeToggle = document.querySelector(".dark-light"),
-    searchToggle = document.querySelector(".searchToggle"),
-    sidebarOpen = document.querySelector(".sidebarOpen"),
-    sidebarClose = document.querySelector(".sidebarClose");
+    searchToggle = document.querySelector(".searchToggle");
 
     let getMode = localStorage.getItem("mode");
         if (getMode && getMode === "dark-mode"){
@@ -26,17 +24,4 @@ const body = document.querySelector("body"),
 // js code to toggle search box
         searchToggle.addEventListener("click" , () => {
         searchToggle.classList.toggle("active");
-    });
-
-// js code to toggle sidebar
-    sidebarOpen.addEventListener("click" , () =>{
-        nav.classList.add("active");
-    });
-
-    body.addEventListener("click" , e =>{
-        let clickedElm = e.target;
-
-        if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
-            nav.classList.remove("active");
-        }
     });
--- Views/Pages/home.php
+++ Views/Pages/home.php
@@ -11,37 +11,43 @@
 <body>
     <img src="Assets/Img/gpp.svg" id="logo_image" alt="">
 
-        <?php include 'Views/Layout/navbar.php'; ?>
+    <?php include 'Views/Layout/navbar.php'; ?>
 
-        <div class="container">
-            <h1 class="title-under-logo">h1asdjsahjf</h1>
+    <div class="container">
+        <h1 class="title-under-logo">Power to the players</h1>
 
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
-            <p>sadbahgdj</p>
+        <div class="row">
+                <p> Breng jouw spel naar het volgende niveau op het grote scherm! Met een privé-theater dat speciaal voor jou en je crew is gereserveerd, heb je nog nooit eerder zo gespeeld. Maak er een toernooi van! </p>
+                <p> Neem je eigen favoriete Xbox One-spellen mee of kies uit het aanbod van je theater. </p>
+                <p>Dingen die je moet weten</p>
 
+
+            <div class="col-md-12">
+            <img src="Assets/Img/001.jpg" alt="" class="img-fluid home-image"></img>
+                    <p class="text-start fw-normal"> Er is geen minimum voor groepen, maar het wordt aanbevolen dat de groepsgrootte tussen de 8 en 12 personen is. Dit zal de speeltijd voor elke speler maximaliseren.
+                    Voel je vrij om je eigen Xbox-spel mee te nemen om te spelen (persoonlijke spelconsoles of spellen voor andere spelconsoles zijn niet toegestaan).
+                    Er is geen leeftijdsgrens voor videospelletjes op Xbox, maar de ouders moeten wel zelf kunnen beslissen over de spelbeoordeling van oudere gamers (d.w.z., titels met een 'M'-rating).
+                    Feesten kunnen maximaal 6 weken voor de datum van uw voorkeur worden aangevraagd en zijn niet gegarandeerd tot de datum is bevestigd en geboekt door het theater.
+                    Voor elke partij kan een aanbetaling van $50 worden gevraagd en kan op elk moment voor de partij aan de kassa worden betaald. De boeking kan pas worden gereserveerd na ontvangst van de aanbetaling.
+                    Annuleringen met een opzegtermijn van minder dan 24 uur kunnen leiden tot een niet-terugvorderbare aanbetaling.
+                    Buiten eten en drinken is niet toegestaan in de theatercomplexen, maar als u een verjaardag viert, kunt u uw eigen verjaardagstaart meenemen! Wij zorgen voor de borden, servetten en bestek.
+                    Feesten kunnen op elk moment buiten de openingsuren geboekt worden. Een voorbeeldboeking is zaterdag 10.00 - 12.00 uur of eender welke nacht na 22.30 uur, in afwachting van beschikbaarheid in bepaalde theaters en kan rechtstreeks bij het theater worden bevestigd.
+                    Cadeaubonnen, alle belangrijke creditcards en debetkaarten worden geaccepteerd als betaalmiddel.
+                    Het is mogelijk dat er op sommige locaties doordeweeks geen partyboekingen beschikbaar zijn.</p>
+
+            </div>
+ 
+            </div>
+
+
+            <div class="col-md-10">
+
+
+
+            </div>
         </div>
+
+    </div>
 
 
 
