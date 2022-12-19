4ea6dc1a6a3049a12154dfdd3af867e1722bc0db
Lennghc <l.m.esveld@gmail.com>
2022-12-19 15:41:36 +0100

    header footer fix

--- Views/Layout/footer.php
+++ Views/Layout/footer.php
@@ -18,21 +18,34 @@
                 </ul>
             </div>
             <div>
-                <h6 class="footer_title">Games</h6>
+                <h6 class="footer_title">Pagina's</h6>
                 <ul class="footer_list">
                     <li class="footer_list-item">
-                        <a href="" class="footer_list-link">namen</a>
+                        <?= isset($_SESSION['user']) ? '<a href="index.php?con=home&op=logout" class="footer_list-link">Uitloggen</a>' : '<a href="index.php?con=home&op=registreer" class="footer_list-link">Registreren</a>'?>
                     </li>
+                    <li class="footer_list-item">
+                        <?= isset($_SESSION['user']) ? '' : '<a href="index.php?con=home&op=login" class="footer_list-link">Inloggen</a>'?>
+                    </li>
+
+
                 </ul>
             </div>
             <div>
                 <h6 class="footer_title">Privacybeleid</h6>
                 <ul class="footer_list">
                     <li class="footer_list-item">
-                        <a href="Views/Policy/termsfeed-cookies-policy-html-english.html" class="footer_list-link">Cookies (EN)</a><br>
-                        <a href="Views/Policy/termsfeed-privacy-policy-html-dutch.html" class="footer_list-link">Privacy (NL)</a><br>
-                        <a href="Views/Policy/termsfeed-privacy-policy-html-english.html" class="footer_list-link">Privacy (EN)</a><br>
-                        <a href="Views/Policy/termsfeed-return-refund-policy-html-english.html" class="footer_list-link">Return & Refund (EN)</a><br>
+                        <a href="Views/Policy/termsfeed-cookies-policy-html-english.html" class="footer_list-link">Cookies (EN)</a>
+                    </li>
+                    <li class="footer_list-item">
+                        <a href="Views/Policy/termsfeed-privacy-policy-html-dutch.html" class="footer_list-link">Privacy (NL)</a>
+                    </li>
+                    <li class="footer_list-item">
+                        <a href="Views/Policy/termsfeed-privacy-policy-html-english.html" class="footer_list-link">Privacy (EN)</a>
+                    </li>
+                    <li class="footer_list-item">
+                        <a href="Views/Policy/termsfeed-return-refund-policy-html-english.html" class="footer_list-link">Return & Refund (EN)</a>
+                    </li>
+                    <li class="footer_list-item">
                         <a href="Views/Policy/termsfeed-terms-conditions-html-english.html" class="footer_list-link">Terms & Conditions (EN)</a>
                     </li>
                 </ul>
@@ -44,17 +57,17 @@
             <ul class="footer-list">
                 <li class="footer_list-item">
                     <a href="https://www.facebook.com/Kinepolis.Nederland" class="footer_list-link">
-                        <li class="fa-brands fa-facebook"></li>
-                    </a>
-                    <a href="https://twitter.com/kinepolis_nl" class="footer_list-link">
-                        <li class="fa-brands fa-twitter"></li>
-                    </a>
-                    <a href="https://www.instagram.com/kinepolis_nl/" class="footer_list-link">
-                        <li class="fa-brands fa-instagram"></li>
-                    </a>
-                    <a href="https://www.youtube.com/channel/UCKyNMhPXbqTQJ-Q7h1zRvLg" class="footer_list-link">
-                        <li class="fa-brands fa-youtube"></li>
-                    </a>
+                <li class="fa-brands fa-facebook"></li>
+                </a>
+                <a href="https://twitter.com/kinepolis_nl" class="footer_list-link">
+                    <li class="fa-brands fa-twitter"></li>
+                </a>
+                <a href="https://www.instagram.com/kinepolis_nl/" class="footer_list-link">
+                    <li class="fa-brands fa-instagram"></li>
+                </a>
+                <a href="https://www.youtube.com/channel/UCKyNMhPXbqTQJ-Q7h1zRvLg" class="footer_list-link">
+                    <li class="fa-brands fa-youtube"></li>
+                </a>
                 </li>
             </ul>
         </div>
--- Views/Layout/header.php
+++ Views/Layout/header.php
@@ -15,10 +15,13 @@
     <link rel="stylesheet" href="<?= PATH_DIR ?>/Assets/Css/footer.css">
     <link rel="stylesheet" href="<?= PATH_DIR ?>/Assets/Css/navbar.css">
     <link rel="stylesheet" href="<?= PATH_DIR ?>/Assets/Css/style.css">
-    <link rel="stylesheet" href="<?= PATH_DIR ?>/Assets/Css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.1/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css" integrity="sha512-6S2HWzVFxruDlZxI3sXOZZ4/eJ8AcxkQH1+JjSe/ONCEqR9L4Ysq5JdT5ipqtzU7WHalNwzwBv+iE51gNHJNqQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js" integrity="sha512-lbwH47l/tPXJYG9AcFNoJaTMhGvYWhVM9YI43CT+uteTRRaiLCui8snIgyAN8XWgNjNhCqlAUdzZptso6OCoFQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <script src="<?= PATH_DIR ?>/Assets/Js/main.js"></script>
