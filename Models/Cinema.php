280809752e0c7a53c07ba21e110a9c6b4e7fca18
Lennghc <l.m.esveld@gmail.com>
2022-12-21 16:57:51 +0100

    Cinema only showed when desc is NOT NULL

--- Models/Cinema.php
+++ Models/Cinema.php
@@ -8,7 +8,18 @@ class Cinema extends Main
     public function all()
     {
         try {
-            $sql = "SELECT cinema_id,cinema_name FROM Cinema";
+            $sql = "SELECT cinema_id,cinema_name,cinema_desc FROM Cinema WHERE cinema_desc IS NOT NULL";
+            $results = self::readsData($sql);
+            return $results;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function read($cinema_id)
+    {
+        try {
+            $sql = "SELECT cinema_id,cinema_name,cinema_desc FROM Cinema WHERE cinema_id = $cinema_id";
             $results = self::readsData($sql);
             return $results;
         } catch (Exception $e) {
