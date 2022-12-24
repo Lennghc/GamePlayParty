f857b6d77579ebc7e545dde1de7e0e46d455220b
Lennghc <l.m.esveld@gmail.com>
2022-12-24 15:24:04 +0100

    Update Lounge.php

--- Models/Lounge.php
+++ Models/Lounge.php
@@ -1,12 +1,24 @@
 <?php
 require_once 'Main.php';
 
-class Lounge extends Main 
+class Lounge extends Main
 {
+
+    public function create($lounge_nmr, $lounge_chair_places,$lounge_wheelchair_places,$lounge_screensize,$lounge_open_date,$lounge_timeslots,$cinema_id)
+    {
+        try {
+            $sql = "INSERT INTO `Lounge` (`lounge_nmr`, `lounge_chair_places`,`lounge_wheelchair_places`,`lounge_screensize`,`lounge_open_date`,`lounge_timeslots`,`cinema_id`) VALUES ('{$lounge_nmr}','{$lounge_chair_places}','{$lounge_wheelchair_places}','{$lounge_screensize}','{$lounge_open_date}','{$lounge_timeslots}','{$cinema_id}')";
+            $result = self::createData($sql);
+            return $result;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
     public function timeSlots($cinema_id)
     {
         try {
-            $sql = "SELECT lounge_open_date, lounge_start_time, lounge_end_time FROM Lounge WHERE `lounge_open_date` >= NOW() + INTERVAL 2 DAY AND `cinema_id` = $cinema_id";
+            $sql = "SELECT lounge_open_date, lounge_timeslots, lounge_id FROM Lounge WHERE `lounge_open_date` >= NOW() + INTERVAL 2 DAY AND `lounge_timeslots` IS NOT NULL AND `cinema_id` = $cinema_id";
             $results = self::readsData($sql);
             return $results;
         } catch (Exception $e) {
@@ -14,4 +26,14 @@ class Lounge extends Main
         }
     }
 
+    public function removeTime($lounge_id,$timeslots)
+    {
+        try {
+            $sql = "UPDATE `Lounge` set `lounge_timeslots` = '$timeslots' WHERE lounge_id = $lounge_id";
+            $results = self::updateData($sql);
+            return $results;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
 }
